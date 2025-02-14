0e9c5f4f8b5ddccc99b7862c481bc03719d47806
Itallo Sávio <itallo.prog@gmail.com>
2025-02-14 20:23:28 -0300

    Change child element to change color of background and border in Card component.

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -1,13 +1,22 @@
 import "./Card.css";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default props => {
+    // const styleColor = {
+    //     backgroundColor: props.color || "#c6c6c6",
+    //     borderColor: props.color || "#c6c6c6",
+    // }
     return (
         <div className="Card">
-            <div className="Title">
+             {/*O style pode ser usado da forma que está ou style={styleColor}*/}
+            <div className="Title" style={{
+                borderColor: props.color || "#c6c6c6",
+                backgroundColor: props.color || "#c6c6c6",
+            }}>
                 <h1>{props.titulo}</h1>
             </div>
             <div className="Content">
-                { props.children }
+                {props.children}
             </div>
         </div>
     )
