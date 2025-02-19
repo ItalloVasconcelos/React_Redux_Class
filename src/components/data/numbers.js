8bf7347caf372a9297ebbea235f0de78901cb3b1
Itallo Sávio <itallo.prog@gmail.com>
2025-02-18 21:55:45 -0300

    Class 08 - Even or Odds numbers

---
+++ src/components/data/numbers.js
@@ -0,0 +1,16 @@
+// id, nome , preço
+// pasta repetição, arquivo tabela produtos
+// criar uma tabela com jsx, mostrando todos os produtos, id e nome de uma lado e preço do outro
+
+
+export default [
+        {id: 1, number:  9},
+        {id: 2, number: 1332},
+        {id: 3, number: 1},
+        {id: 4, number: 0},
+        {id: 5, number: 123256},
+        {id: 6, number: 5423},
+        {id: 7, number: 10},
+        {id: 8, number: 32},
+        {id: 9, number: 341},
+    ];
