3262849860b7acec51d9dc559b26638a878811f0
Itallo Sávio <itallo.prog@gmail.com>
2025-02-20 22:55:22 -0300

    Class 08 - The use of If with component.

---
+++ src/components/conditionals/InfoUser.jsx
@@ -0,0 +1,16 @@
+import If from './If'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+    const user = props.user || {}
+    return (
+        <div>
+            <If test={user && user.name}>
+                Seja bem vindo <strong>{user.name}</strong> !
+            </If>
+            <If test={!user || !user.name}>
+                Seja bem vindo <strong>Unknow</strong> !
+            </If>
+        </div>
+    );
+}
